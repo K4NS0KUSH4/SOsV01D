798e2758eefb8452e8fd4908eacd68ba91ca840d
Cloud User <ec2-user@ip-172-31-43-194.ec2.internal>
2023-08-31 12:21:46 +0000

    Little changes in exercicio05.c

--- Lab01bIntroducaoAoC/exercicio05.c
+++ Lab01bIntroducaoAoC/exercicio05.c
@@ -10,6 +10,16 @@ void lineTimesMatrix(int num, int line, int rows, int columns, int matrix[rows][
 	}
 }
 
+void columnTimesMatrix(int num, int column, int rows, int columns, int matrix[rows][columns]) {
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < columns; j++) {
+			if(column - 1 == j) {
+				matrix[i][j] = matrix[i][j] * num;
+			}
+		}
+	}
+}
+
 void printMatrix(int rows, int columns, int matrix[rows][columns]) {
 	for(int i = 0; i < rows; i++) {
 		for(int j = 0; j < columns; j++) {
@@ -22,11 +32,18 @@ void printMatrix(int rows, int columns, int matrix[rows][columns]) {
 
 int main(void) {
 	printf("|----------//---------- EXERCÍCIO 05 ----------//----------|\n");
-	int bidimensionalArray[2][3] = {{9, 8, 7}, {6, 5, 4}};
-	printMatrix(2, 3, bidimensionalArray);
+	int bidimensionalArrayA[2][3] = {{9, 8, 7}, {6, 5, 4}};
+	int bidimensionalArrayB[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+	printf("Matriz original\n");
+	printMatrix(2, 3, bidimensionalArrayA);
 	printf("\nMultiplicando os elementos da segunda linha por 3:\n");
-	lineTimesMatrix(3, 2, 2, 3, bidimensionalArray);
-	printMatrix(2, 3, bidimensionalArray);
+	lineTimesMatrix(3, 2, 2, 3, bidimensionalArrayA);
+	printMatrix(2, 3, bidimensionalArrayA);
+	printf("\nMatriz original\n");
+	printMatrix(4, 3, bidimensionalArrayB);
+	printf("\nMultiplicando os elementos da terceira coluna por 5:\n");
+	columnTimesMatrix(5, 3, 4, 3, bidimensionalArrayB);
+	printMatrix(4, 3, bidimensionalArrayB);
 	printf("|----------//---------- //----------// ----------//----------|\n\n");
 
 	return 0;
