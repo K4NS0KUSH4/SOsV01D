49411ff30b6e430930c4d4a5470b7d1a51a5d616
Cloud User <ec2-user@ip-172-31-43-194.ec2.internal>
2023-09-07 07:48:43 +0000

    Adicionando método alternativo para impressão da mensagem de saída (utilização de printf, sem stdout)

--- Lab02aProcessos/stdoutProcessChain.c
+++ Lab02aProcessos/stdoutProcessChain.c
@@ -7,6 +7,8 @@ int main(int argc, char *argv[]) {
 	int i, n;
 
 	if(argc != 2) {
+		//Caso queira utilizar printf:
+		//printf("Usage: %s processes\n", argv[0]);
 		fprintf(stdout, "Usage: %s processes\n", argv[0]);
 		return 1;
 	}
@@ -17,6 +19,8 @@ int main(int argc, char *argv[]) {
 		if(childpid = fork())
 			break;
 
+	//Caso queira utilizar printf:
+	//printf("i: %d  process ID:%ld  parent ID:%ld  child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
 	fprintf(stdout, "i:%d  process ID:%ld  parent ID:%ld  child ID:%ld\n",
 		 i, (long)getpid(), (long)getppid(), (long)childpid);
 
